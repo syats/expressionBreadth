c5054dd5ecb81840a3477c1f51122aeda3539e30
syats <syats.vm@gmail.com>
2019-03-16 16:11:51 +0100

    Changed histogram visualization

--- computeHistograms.py
+++ computeHistograms.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 from importlib import reload
 import math
 import os.path
@@ -8,90 +9,120 @@ import pickle
 import readers as rs
 import utils as us
 
+# each directory is a tuple (path,extension,fileType,prefix). All files ending in said extension in said path will be
+#  passed to readers.reader with ftype=fileType. Therefore fileType must be one of the kown filetypes, see readers.py
+# for a list of them. Prefix will be used internally to name the data sets.
 
-
-
-#each directory is a tuple (path,extension,fileType,prefix). All files ending in said extension in said path will be passed to readers.reader with ftype=fileType. Therefore fileType must be one of the kown filetypes, see readers.py for a list of them. Prefix will be used internally to name the data sets.
-dirs = [ ('../otherArticleWork/expressionAtlas_baseline/','tsv','expressionAtlas','ExAt_') ]
-dirs = [ ('../otherArticleWork/expressionAtlas_baseline/small/','tsv','expressionAtlas','ExAtS_') ] #small for testing
-dirs = [('../otherArticleWork/Jaitin2014/','umitab.txt','Jaitin','Jaitin')]
+dirs = [('./data/expressionAtlas/', 'tsv', 'expressionAtlas', 'ExAt_')]
+toskip = ["MTAB2512"]
+#dirs = [
+#    ('../otherArticleWork/expressionAtlas_baseline/small/', 'tsv', 'expressionAtlas', 'ExAtS_')]  # small for testing
+#dirs = [('../otherArticleWork/Jaitin2014/', 'umitab.txt', 'Jaitin', 'Jaitin')]
 outputDir = './outData/'
 forceRead = False;
 useReplicates = True;
 minNumTissues = 6;
 
-
 sufix = "" if useReplicates else "_NR"
 allCs = dict();
-#Maybe we've already read some of the Files, so we take a look at outputDir
+# Maybe we've already read some of the Files, so we take a look at outputDir
 if not forceRead:
-	for di in dirs:
-		dictFileName = outputDir+di[3]+"MATRICES"+sufix+".pkl"
-		if os.path.isfile(dictFileName):
-				d = pickle.load(open(dictFileName,'rb'));
-				for ke,C in d.items():
-					allCs[ke] = C;
-
+    for di in dirs:
+        dictFileName = outputDir + di[3] + "MATRICES" + sufix + ".pkl"
+        if os.path.isfile(dictFileName):
+            d = pickle.load(open(dictFileName, 'rb'));
+            for ke, C in d.items():
+                allCs[ke] = C;
 
 for di in dirs:
-	count=-1;
-	readCount = 0;
-	fileList = us.getFilesFromDir(di[0],di[1]);
-	thisCs = dict()
-	for fi in fileList:
-		expName = fi[:-len(di[1])-1].split('-')
-		if len(expName) >= 3:
-			orName = expName[1]+expName[2];
-		else:
-			orName = expName[0]
-		count+=1;
-		print("file "+str(count)+"/"+str(len(fileList))+" :"+fi),
-		if di[3]+orName in allCs.keys():
-			print('...loaded')
-			thisCs[di[3]+orName] = allCs[di[3]+orName];
-			continue
-		C = rs.reader(di[0]+fi,di[2],useReplicates=useReplicates);
-		readCount += 1;
-		#print('....read '+str(C.shape))
-		if isinstance(C,np.ndarray):
-			if min(C.shape) < minNumTissues:
-				continue
-			thisCs[di[3]+orName]=C
-			allCs[di[3]+orName]=C
-		else:
-			for ke,ma in C.items():
-				if min(ma.shape) < minNumTissues:
-					continue
-				thisCs[di[3]+orName+ke] = ma
-				allCs[di[3]+orName+ke]  = ma
+    count = -1;
+    readCount = 0;
+    fileList = us.getFilesFromDir(di[0], di[1]);
+    thisCs = dict()
+    for fi in fileList:
+        expName = fi[:-len(di[1]) - 1].split('-')
+        if len(expName) >= 3:
+            orName = expName[1] + expName[2];
+        else:
+            orName = expName[0]
+        count += 1;
+        print("file " + str(count) + "/" + str(len(fileList)) + " : " + fi),
+        if di[3] + orName in allCs.keys():
+            print('...loaded')
+            thisCs[di[3] + orName] = allCs[di[3] + orName];
+            continue
+        print("Skipped:  ",orName)
+        continue
+        C = rs.reader(di[0] + fi, di[2], useReplicates=useReplicates);
+        readCount += 1;
+        # print('....read '+str(C.shape))
+        if isinstance(C, np.ndarray):
+            if min(C.shape) < minNumTissues:
+                continue
+            thisCs[di[3] + orName] = C
+            allCs[di[3] + orName] = C
+        else:
+            for ke, ma in C.items():
+                if min(ma.shape) < minNumTissues:
+                    continue
+                thisCs[di[3] + orName + ke] = ma
+                allCs[di[3] + orName + ke] = ma
 
-
-
-	if readCount > 0:
-		pickle.dump(thisCs,open(outputDir+di[3]+"MATRICES"+sufix+".pkl",'wb'))
-
-
-
+    if readCount > 0:
+        pickle.dump(thisCs, open(outputDir + di[3] + "MATRICES" + sufix + ".pkl", 'wb'))
 
 numHists = len(allCs)
 numSubplotCols = 1;
 numSubplotRows = 1;
 if numHists > 2:
-	numSubplotCols = 2;
-	numSubplotRows = math.ceil(numHists/2);
+    numSubplotCols = 2;
+    numSubplotRows = math.ceil(numHists / 2);
 
 subPlotNum = 1;
-for ke,C in allCs.items():
-	su = C.sum(axis=1);
-	numTissues = min(C.shape)
-	hbins = [float(i)+0.5 for i in range(numTissues+1)]
-	plt.subplot(numSubplotRows,numSubplotCols,subPlotNum)
-	nA,nB,nC = plt.hist(su,hbins)
+allkeys = list(allCs.keys())
+allkeys.sort()
+for ke in  allkeys:
+    C = allCs[ke]
+    if any([ke.endswith(x) for x in toskip]):
+        print("Skipping ", ke)
+        continue
+    su = C.sum(axis=1);
+    numTissues = min(C.shape)
+    img = np.array([(su==i).sum()/float(max(C.shape)) for i in range(1, numTissues+1)])
+    #plt.subplot(numSubplotRows, numSubplotCols, subPlotNum)
 
-	plt.xlim([-0.5,numTissues+.5])
+    fig = plt.figure(1)
+    plt.subplot(numSubplotRows, numSubplotCols, subPlotNum)
+    im = plt.imshow(img[np.newaxis,:],interpolation='none',aspect='auto')
+    if  subPlotNum in [len(allkeys)-2, len(allkeys)-1]:
+        if subPlotNum == len(allkeys)-1:
+            off = float(numTissues)/20
+        else:
+            off = 0
+        plt.xticks([off+i*(float(numTissues)/10) for i in range(10)],
+                   ["{:.1f}".format(0.1+i*0.1)  for i in range(10)])
+        plt.xlabel("Proportion of tissues",fontsize=18)
+    else:
+        plt.xticks([])
+    plt.yticks([])
+    h = plt.ylabel(ke, labelpad=45, rotation=0)
+    fig.gca().yaxis.set_label_coords(-0.1,0)
 
-	plt.xticks(range(numTissues+1))
-	plt.text(numTissues/3,max(nA)*0.5,ke)
-	subPlotNum += 1;
+
+    #
+    # plt.figure(2)
+    # plt.subplot(numSubplotRows, numSubplotCols, subPlotNum)
+    # hbins = [float(i) + 0.5 for i in range(numTissues + 1)]
+    # nA, nB, nC = plt.hist(su, hbins)
+    # plt.xlim([-0.5, numTissues + .5])
+    # plt.xticks(range(numTissues + 1))
+    # plt.text(numTissues / 3, max(nA) * 0.5, ke)
+
+    subPlotNum += 1;
+
+cbar_ax = fig.add_axes([0.952, 0.15, 0.02, 0.7])
+fig.colorbar(im, cax=cbar_ax)
+cbar_ax.set_title("Proportion\nof genes",fontsize=16)
+plt.subplots_adjust(left=0.08,right=0.92,top=0.97,bottom=0.05,wspace=0.25,hspace=0.20)
 
 plt.show();
--- readers.py
+++ readers.py
@@ -1,147 +1,144 @@
-#Author: Victor Mireles
+# Author: Victor Mireles
 
-#This module provides a set of readers for diverse gene expression formats. It also provides a wrapper method for ease of use.
+# This module provides a set of readers for diverse gene expression formats. It also provides a wrapper method for ease of use.
 
-#All of these readers return a m x n binary matrix, where m is the number of genes and n is the number of conditions for which the expression is measured.
+# All of these readers return a m x n binary matrix, where m is the number of genes and n is the number of conditions for which the expression is measured.
 
 import numpy as np
 import csv
 
 
-def reader(fileName,ftype,binarized=True,useReplicates=True):
-	if ftype == 'expressionAtlas':
-		return expressionAtlas(fileName,binarized=binarized,useReplicates=useReplicates)
-	if ftype == 'Jaitin':
-		p1 = fileName.split("/")[:-1];
-		filePath = "/".join(p1)+"/"
-		return jaitin2014(filePath,binarized=binarized)
+def reader(fileName, ftype, binarized=True, useReplicates=True):
+    if ftype == 'expressionAtlas':
+        return expressionAtlas(fileName, binarized=binarized, useReplicates=useReplicates)
+    if ftype == 'Jaitin':
+        p1 = fileName.split("/")[:-1];
+        filePath = "/".join(p1) + "/"
+        return jaitin2014(filePath, binarized=binarized)
 
 
+def jaitin2014(filePath, binarized=True):
+    # First we read the metadata
+    metadataName = filePath + "GSE54006_experimental_design.txt";
+    numSamples = 0;
+    samplesPerType = dict();
+    filePointer = open(metadataName, 'r');
+    cr = csv.reader(filePointer, delimiter='\t')
+    toSkip = 1;
+    for row in cr:
+        toSkip -= 1;
+        if toSkip >= 0:
+            continue
+        sampleId = row[14];
+        if sampleId == "ommited":
+            continue
 
-def jaitin2014(filePath,binarized=True):
+        numSamples += 1
+        sampleType = row[11];
+        if sampleType in samplesPerType.keys():
+            samplesPerType[sampleType].append(sampleId)
+        else:
+            samplesPerType[sampleType] = [sampleId];
 
-	#First we read the metadata
-	metadataName = filePath+"GSE54006_experimental_design.txt";
-	numSamples = 0;
-	samplesPerType = dict();
-	filePointer = open(metadataName,'r');
-	cr = csv.reader(filePointer,delimiter='\t')
-	toSkip = 1;
-	for row in cr:
-		toSkip -= 1;
-		if toSkip >= 0:
-			continue
-		sampleId = row[14];
-		if sampleId == "ommited":
-			continue
+    # Now we read the data and sort it into the different C matrices
 
-		numSamples+=1
-		sampleType = row[11];
-		if sampleType in samplesPerType.keys():
-			samplesPerType[sampleType].append(sampleId)
-		else:
-			samplesPerType[sampleType] = [sampleId];
+    dataFileName = filePath + "GSE54006_umitab.txt";
+    numGenes = getNumLines(dataFileName) - 1;
 
-	#Now we read the data and sort it into the different C matrices
+    # Initialize the matrices
+    allMatrices = dict()
+    colsPerSample = dict();
+    for sampleType, sampleIndices in samplesPerType.items():
+        allMatrices[sampleType] = np.zeros((numGenes, len(sampleIndices)));
+        colsPerSample[sampleType] = []
 
-	dataFileName = filePath + "GSE54006_umitab.txt";
-	numGenes   = getNumLines(dataFileName) - 1;
+    filePointer = open(dataFileName, 'r')
+    rowNum = 0;
+    cr = csv.reader(filePointer, delimiter='\t')
+    goodGenes = np.zeros(numGenes);
+    for row in cr:
+        rowNum += 1;
+        if rowNum == 1:
+            numCols = len(row);
+            for col in range(1, numCols):
+                for sampleType, sampleIndices in samplesPerType.items():
+                    if row[col] in sampleIndices:
+                        colsPerSample[sampleType].append(int(col))
 
-	#Initialize the matrices
-	allMatrices = dict()
-	colsPerSample = dict();
-	for sampleType,sampleIndices in samplesPerType.items():
-		allMatrices[sampleType] = np.zeros((numGenes,len(sampleIndices)));
-		colsPerSample[sampleType] = []
+            continue
+        if rowNum > numGenes:
+            print(rowNum)
+            continue
+        for sampleType, colIndices in colsPerSample.items():
+            vect = np.array([int(row[x]) for x in colIndices]);
+            allMatrices[sampleType][rowNum - 2, :] = vect
+            if vect.sum() > 0:
+                goodGenes[rowNum - 2] = 1;
 
-	filePointer = open(dataFileName,'r')
-	rowNum = 0;
-	cr = csv.reader(filePointer,delimiter='\t')
-	goodGenes = np.zeros(numGenes);
-	for row in cr:
-		rowNum += 1;
-		if rowNum == 1:
-			numCols = len(row);
-			for col in range(1,numCols):
-				for sampleType,sampleIndices in samplesPerType.items():
-					if row[col] in sampleIndices:
-						colsPerSample[sampleType].append(int(col))
+    nz = np.nonzero(goodGenes)[0]
+    for sampleType, C in allMatrices.items():
+        if binarized:
+            allMatrices[sampleType] = binarize(C[nz, :])
+        else:
+            allMatrices[sampleType] = C[nz, :]
 
-
-			continue
-		if rowNum > numGenes:
-			print(rowNum)
-			continue
-		for sampleType,colIndices in colsPerSample.items():
-			vect = np.array([int(row[x]) for x in colIndices]);
-			allMatrices[sampleType][rowNum-2,:] = vect
-			if vect.sum() > 0:
-				goodGenes[rowNum-2]  = 1;
-
-	nz = np.nonzero(goodGenes)[0]
-	for sampleType,C in allMatrices.items():
-		if binarized:
-			allMatrices[sampleType] = binarize(C[nz,:])
-		else:
-			allMatrices[sampleType] = C[nz,:]
-
-
-	return allMatrices
+    return allMatrices
 
 
+def expressionAtlas(fileName, binarized=True, colsToIgnore=2, useReplicates=True):
+    numGenes = getNumLines(fileName)
+    filePointer = open(fileName, 'r')
+    cr = csv.reader(filePointer, delimiter='\t')
+    toSkip = 1;
+    numSamples = None
+    skipped = 0;
+    for row in cr:
+        if row[0][0] == '#':
+            skipped += 1;
+            continue
+        toSkip = toSkip - 1 if toSkip >= 0 else -1;
+        if toSkip >= 0:
+            skipped += 1;
+            continue
+        if numSamples == None:
+            if useReplicates:
+                numSamples = len(row) - colsToIgnore;
+                numCols = numSamples;
+            else:
+                numSamples = 0;
+                numCols = len(row) - colsToIgnore;
+                for i in range(colsToIgnore, len(x)):
+                    numSamples += len(row[i].split(','));
+            C = np.zeros((numGenes, numSamples));
+            numGene = 0;
+        try:
+            thisVect = [row[i] if row[i] != "" else 0 for i in range(colsToIgnore, numCols + colsToIgnore)]
+            if useReplicates:
+                thisVectF = [float(np.mean([float(xx) for xx in ff.split(',')])) if ',' in ff else float(ff) for ff in
+                             thisVect]
+            else:
+                thisVectF1 = [[float(xx) if xx != "" else 0 for xx in ff.split(',')] if ',' in ff else [float(ff)] for
+                              ff in thisVect]
+                thisVectF = [item for sublist in thisVectF1 for item in sublist]
 
-def expressionAtlas(fileName,binarized=True,colsToIgnore=2,useReplicates=True):
-	numGenes   = getNumLines(fileName)
-	filePointer = open(fileName,'r')
-	cr  = csv.reader(filePointer,delimiter='\t')
-	toSkip = 1;
-	numSamples = None
-	skipped = 0;
-	for row in cr:
-		if row[0][0] == '#':
-			skipped += 1;
-			continue
-		toSkip = toSkip - 1 if toSkip >= 0 else -1;
-		if toSkip >= 0:
-			skipped += 1;
-			continue
-		if numSamples == None:
-			if useReplicates:
-				numSamples = len(row) - colsToIgnore;
-				numCols = numSamples;
-			else:
-				numSamples = 0;
-				numCols = len(row) - colsToIgnore;
-				for i in range(colsToIgnore,len(row)):
-					numSamples+=len(row[i].split(','));
-			C = np.zeros((numGenes,numSamples));
-			numGene = 0;
-		try:
-			thisVect = [row[i] if row[i]!="" else 0 for i in range(colsToIgnore,numCols+colsToIgnore)]
-			if useReplicates:
-				thisVectF = [float(np.mean([float(xx) for xx in ff.split(',')])) if ',' in ff else float(ff) for ff in thisVect]
-			else:
-				thisVectF1 = [[float(xx) if xx != "" else 0 for xx in ff.split(',')] if ',' in ff else [float(ff)] for ff in thisVect]
-				thisVectF = [item for sublist in thisVectF1 for item in sublist]
+            C[numGene, :] = np.array(thisVectF)
+        except ValueError:
+            skipped += 1;
+            continue
+        numGene += 1;
 
-			C[numGene,:] = np.array(thisVectF)
-		except ValueError:
-			skipped+=1;
-			continue
-		numGene += 1;
-
-	#If we skipped some lines, the matrix is probably too large.
-	C = C[:numGenes-skipped,:];
-	if binarized:
-		return binarize(C);
-	return C
-
+    # If we skipped some lines, the matrix is probably too large.
+    C = C[:numGenes - skipped, :];
+    if binarized:
+        return binarize(C);
+    return C
 
 
 def getNumLines(fileName):
-	return sum(1 for line in open(fileName))
+    return sum(1 for line in open(fileName))
 
-def binarize(D,threshold=0):
-	C = np.zeros_like(D);
-	C[np.nonzero(D>threshold)] = 1;
-	return C
+
+def binarize(D, threshold=0):
+    C = np.zeros_like(D);
+    C[np.nonzero(D > threshold)] = 1;
+    return C
